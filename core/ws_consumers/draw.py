8c940416eb9421f802b35966f64317a59d112e60
Zach Todd <zachtodd@pop-os.localdomain>
2024-01-07 15:09:55 -0500

    add comments

--- core/ws_consumers/draw.py
+++ core/ws_consumers/draw.py
@@ -8,6 +8,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
+# An initial message to send to OpenAI that explains the concept of the
+# game and the kind of responses we want.
 system_prompt = """
 There is a game in which two players both work to complete a simple drawing
 according to a series of prompts that build on one another.
@@ -28,17 +30,32 @@ should be an array of strings.
 
 
 class DrawConsumer(AsyncWebsocketConsumer):
-    countdown_time = 30
-    prompts_fetched = False
-    countdown_active = False
-
+    # The game prompts are fetched from OpenAI when the first player connects.
+    # We need to avoid re-fetching when the second player connects, and also
+    # make sure both players are sent their prompts at the same time.
+    # The first player to connect will acquire the fetch_lock while the second
+    # player will block on fetch_lock until the prompts are ready.
     fetch_lock = asyncio.Lock()
 
+    prompts_fetched = False
+
+    countdown_time = 30
+    countdown_active = False
+
+    # Track the connection count as an easy way to know which set of prompts
+    # to send when a new connection is established.
     connection_count = 0
+
     player1_prompts = []
     player2_prompts = []
 
     async def start_countdown(self):
+        """
+        Starts a countdown timer and sends an update of how many seconds
+        remain to each connected Websocket client.
+
+        The timer will restart when it reaches zero.
+        """
         if not DrawConsumer.countdown_active:
             DrawConsumer.countdown_active = True
             while DrawConsumer.countdown_time > 0:
@@ -56,6 +73,13 @@ class DrawConsumer(AsyncWebsocketConsumer):
                     DrawConsumer.countdown_time = 30
 
     async def fetch_prompts(self):
+        """
+        Fetch prompts from the OpenAI API.
+
+        The first caller will trigger the API request while subsequent calls
+        block on the fetch_lock, eventually returning immediately because the
+        prompts_fetched bool has been set to True.
+        """
         async with DrawConsumer.fetch_lock:
             if not DrawConsumer.prompts_fetched:
                 DrawConsumer.prompts_fetched = True
@@ -77,6 +101,10 @@ class DrawConsumer(AsyncWebsocketConsumer):
                 DrawConsumer.player2_prompts = data["player2"]
 
     async def connect(self):
+        """
+        Set up Websocket connection and make OpenAI request if this is the
+        first connection.  Distributes prompts to players.
+        """
         DrawConsumer.connection_count += 1
 
         await self.channel_layer.group_add("draw_group", self.channel_name)
@@ -101,9 +129,11 @@ class DrawConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard("draw_group", self.channel_name)
 
     async def receive(self, text_data):
+        """
+        Broadcast messages received from one player back to themselves and to
+        the other player in the game.
+        """
         data = json.loads(text_data)
-
-        data["countdown"] = DrawConsumer.countdown_time
         await self.channel_layer.group_send(
             "draw_group", {"type": "draw_message", "message": data}
         )
